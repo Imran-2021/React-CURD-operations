4aeb4484de30ea6660dfe1e45161ad067a6cb8b2
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-05-19 13:25:08 +0600

    firt commit

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+        <div className="py-4">
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio quidem provident iusto aut magni, officiis modi minus tenetur, accusantium id eum, perspiciatis quas adipisci aliquid tempore. Excepturi repellendus dolorum sunt?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem necessitatibus expedita sunt soluta voluptatibus aperiam enim, quia ipsa! Exercitationem, adipisci consequuntur laboriosam cumque sed magnam cum eum officia placeat et?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptates totam provident odio non eius impedit soluta at, laudantium tenetur labore fugiat quaerat, cupiditate voluptas. Maxime laboriosam libero adipisci quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem numquam quo similique esse debitis! Consectetur voluptatem iure sunt voluptate, porro nemo repudiandae excepturi ab nostrum, quod soluta earum amet in!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio quidem provident iusto aut magni, officiis modi minus tenetur, accusantium id eum, perspiciatis quas adipisci aliquid tempore. Excepturi repellendus dolorum sunt?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem necessitatibus expedita sunt soluta voluptatibus aperiam enim, quia ipsa! Exercitationem, adipisci consequuntur laboriosam cumque sed magnam cum eum officia placeat et?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptates totam provident odio non eius impedit soluta at, laudantium tenetur labore fugiat quaerat, cupiditate voluptas. Maxime laboriosam libero adipisci quibusdam.</p>
+            
+        </div>
+        </div>
+    );
+};
+
+export default About;
